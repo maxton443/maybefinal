bb6a1be429d4592b3dbe053c382401508f3fffaa
maxton443 <asadul43255@gmail.com>
2025-07-18 19:33:30 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
+# main.py
+
 from telegram.ext import ApplicationBuilder, CommandHandler
 from handlers.start import start_command
 from handlers.menu import menu_handler
 from handlers.admin import admin_handler
 import asyncio
 
-TOKEN = "7773509344:AAFS6Pfbgtz48NQ2januQoLjDhs-eZ_w1w0"  # ✅ সরাসরি টোকেন
+TOKEN = "7773509344:AAFS6Pfbgtz48NQ2januQoLjDhs-eZ_w1w0"  # এখানে তোমার টোকেন
 
 app = ApplicationBuilder().token(TOKEN).build()
 
-# হ্যান্ডলার রেজিস্ট্রেশন
 app.add_handler(CommandHandler("start", start_command))
 app.add_handler(CommandHandler("menu", menu_handler))
 app.add_handler(CommandHandler("admin", admin_handler))
