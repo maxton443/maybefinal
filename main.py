a2b88bf98796e6065b682e38bb45a8d24443e6ab
maxton443 <asadul43255@gmail.com>
2025-07-18 19:05:31 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+from telegram.ext import ApplicationBuilder, CommandHandler
+from handlers.start import start_command
+from handlers.menu import menu_handler
+from handlers.admin import admin_handler
+import os
+import asyncio
+
+TOKEN = "7773509344:AAFS6Pfbgtz48NQ2januQoLjDhs-eZ_w1w0"  # GitHub Secret থেকে নিবে
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+# হ্যান্ডলার রেজিস্ট্রেশন
+app.add_handler(CommandHandler("start", start_command))
+app.add_handler(CommandHandler("menu", menu_handler))
+app.add_handler(CommandHandler("admin", admin_handler))
+
+if __name__ == "__main__":
+    asyncio.run(app.run_polling())
